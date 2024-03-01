19f80ec09320c008adf74d268ab1f197c88e7c63
gopalk26 <gopalakrishnakimid@gmail.com>
2024-03-01 17:12:32 +0530

    Fix change api

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -27,7 +27,7 @@ const response = await fetch(proxyUrl);
 
       
 
-      const newCategories = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      const newCategories = json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
         (c) => c.card?.card?.['@type'] === MENU_ITEM_TYPE_KEY || c.card?.card?.['@type'] === "type.googleapis.com/swiggy.presentation.food.v2.NestedItemCategory"
       );
      
